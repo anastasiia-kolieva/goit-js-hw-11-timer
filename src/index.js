c5003d56aa08024692e688c535f5c0d00a3fdb2b
anastasiia-kolieva <nastya1110@gmail.com>
2020-09-25 09:34:54 +0300

    almost end third task. Can not find the reason, why clearInterwal does not work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Предварительно установленную дату взяла 31 декабря 2020 года
 // количество знаков оставшихся дней определила 3 (потому что в году не более 366 дней)
 // Не могу понять зачем в условии указан selector: '#timer-1', зачем/где его использовать?????
+// Не удалось сделать очисту clearInterval
 
 const daysRef = document.querySelector('span[data-value="days"]');
 const hoursRef = document.querySelector('span[data-value="hours"]');
@@ -13,21 +14,26 @@ class CountdownTimer {
     this.targetDate = targetDate;
   }
 
-  interval = setInterval(() => {
+  startInterval = setInterval(() => {
     const currentDate = Date.now();
-    // число уникальное время 1600968351389
     const time = this.targetDate.getTime() - currentDate;
     // разница между установленной будущей датой и текущим временем
 
     updateTime(time);
   }, 1000);
+
+  stopInterval() {
+    // Пытаюсь сделать условие остановки setInterval. Не выходит, идёт отсчёт в минус, не находит intervalId в 27 строке
+    if (time <= 0) {
+      clearInterval(startInterval);
+      updateTime(0);
+    }
+  }
 }
 
 function updateTime(time) {
   const days = String(Math.floor(time / (1000 * 60 * 60 * 24))).padStart(3,'0');
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
+  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
